0baddab2cead25e1bc74448bb25533bb6eedc1b0
Endrew 'Drew' Barbosa <48924728+DrewBarbosa@users.noreply.github.com>
2020-03-05 18:56:50 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const server = express();
+
+mongoose.connect(
+  "mongodb+srv://drew:PDrNunwcTfBpRS2J@cluster0-ztfes.mongodb.net/test?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
+server.use(express.json);
+server.use(routes);
+server.listen(5000);
